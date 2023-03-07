7e455cc5a622b12ee6f9c1985a1a38de11d20c37
tanglihao <lihao.tang@eeoa.com>
2023-03-07 10:56:06 +0800

    udpate

--- cmd/etcdv3_upsync_proxy/main.go
+++ cmd/etcdv3_upsync_proxy/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,30 +15,30 @@ import (
 func main() {
 	err, cleanup := app.InitApplication()
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	defer cleanup()
 
+	fmt.Println(`sys interrupt :`, start())
+}
+
+func start() error {
 	g := gin.Default()
 	app.RegisterRouter(g)
 
-	// 框架错误捕获
-	errChan := make(chan error)
+	ech := make(chan error)
 	go func() {
 		err := endless.ListenAndServe(":2381", g)
 		if err != nil {
-			errChan <- err
+			ech <- err
 		}
 	}()
 
-	// 系统信号捕获
 	go func() {
 		sign := make(chan os.Signal)
 		signal.Notify(sign, syscall.SIGINT, syscall.SIGTERM)
-		errChan <- fmt.Errorf("%s", <-sign)
+		ech <- fmt.Errorf("%s", <-sign)
 	}()
 
-	// 输出错误消息
-	e := <-errChan
-	fmt.Println(`sys interrupt :`, e)
+	return <-ech
 }
